238e221b92eb09c5722252fdcde25a1f2c1d876c
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-01-28 20:27:33 +0100

    deploy

---
+++ public/precache-manifest.7421412bc688d4c6fb0d5db85b7f0e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "299b1a0eff525dfd88be",
+    "url": "/static/js/main.299b1a0e.chunk.js"
+  },
+  {
+    "revision": "97224eee8177b29d0f20",
+    "url": "/static/js/1.97224eee.chunk.js"
+  },
+  {
+    "revision": "299b1a0eff525dfd88be",
+    "url": "/static/css/main.13de37ae.chunk.css"
+  },
+  {
+    "revision": "7cf2464ac1ca9a46f7af47ca23867ecb",
+    "url": "/index.html"
+  }
+];
